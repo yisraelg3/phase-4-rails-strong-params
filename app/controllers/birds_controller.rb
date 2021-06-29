47ad019b05a519df10864215e159dc3f8ccc83ca
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-28 22:36:18 -0400

    commit tree 8fe0767e6b26820c0afb814813cf2679df6c252e

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
 
+  
   # GET /birds
   def index
     birds = Bird.all
@@ -8,7 +9,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +21,12 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
